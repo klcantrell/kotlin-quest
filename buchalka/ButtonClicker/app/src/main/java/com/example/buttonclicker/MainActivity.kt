2aac15d3be7be14e22c2dcbc715749efc1f30e91
Kal Cantrell <klcantrell@sep.com>
2021-11-16 22:11:10 -0500

    buchalka/buttonClicker: the android lifecycle

--- buchalka/ButtonClicker/app/src/main/java/com/example/buttonclicker/MainActivity.kt
+++ buchalka/ButtonClicker/app/src/main/java/com/example/buttonclicker/MainActivity.kt
@@ -2,18 +2,23 @@ package com.example.buttonclicker
 
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENTS = "TextContent"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var userInput: EditText
     private lateinit var button: Button
     private lateinit var textView: TextView
-    private var numItemsClicked: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,10 +27,57 @@ class MainActivity : AppCompatActivity() {
         textView = findViewById(R.id.textView)
         textView.movementMethod = ScrollingMovementMethod()
 
-        textView.text = "The button got tapped 0 times...\n"
+        textView.text = ""
+        userInput.text.clear()
+
         button.setOnClickListener {
-            numItemsClicked += 1
-            textView.append("The button got tapped $numItemsClicked ${if (numItemsClicked > 1) "times" else "time"}!\n")
+            Log.d(TAG, "onClick called")
+            textView.append(userInput.text)
+            textView.append("\n")
+            userInput.text.clear()
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart called")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState called")
+        val savedString = savedInstanceState.getString(TEXT_CONTENTS, "")
+        textView.text = savedString
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState called")
+        outState.putString(TEXT_CONTENTS, textView.text.toString())
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
     }
 }
